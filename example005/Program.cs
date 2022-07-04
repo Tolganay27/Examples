acf392b84051c0f784e9bd229ade27c38ad6f11f
Tolganay <toki-tok9900@mail.ru>
2022-07-04 21:34:28 +0600

    Add example005 Condition if else

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "толганай")
+{
+    Console.WriteLine("Ура, это же Токи!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
